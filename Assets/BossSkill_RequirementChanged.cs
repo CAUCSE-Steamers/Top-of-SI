a2060ed2c29f81632846c1b004b5f02bd3a838f9
RedChiken <gombabo1217@gmail.com>
2018-09-26 13:45:31 +0900

    BossSkill_Update

---
+++ Assets/BossSkill_RequirementChanged.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Assets;
+
+public class BossSkill_RequirementChanged : MonoBehaviour, BossSkill
+{
+    private Animator anim;
+
+    void BossSkill.Do()
+    {
+        anim.Play("Attack");
+    }
+
+    // Use this for initialization
+    void Start ()
+    {
+        anim = GetComponent<Animator>();
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            anim.Play("Attack");
+        }
+        // Temporary If for testing Animation.
+        // 1. Add Damage Affect to Character in BossSkill.Do
+    }
+}
---
+++ Assets/Skill.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Assets
+{
+    interface BossSkill
+    {
+        void Do();       // 프로그래머 객체 배열
+    }
+
+    interface ProgrammerSkill
+    {
+        void Do();
+    }
+}
