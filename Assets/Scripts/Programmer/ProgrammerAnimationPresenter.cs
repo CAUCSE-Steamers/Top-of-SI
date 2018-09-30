230eb011856c9c50be883f189330be9ddfc91913
nErumin <nerumin90@gmail.com>
2018-09-30 20:16:36 +0900

    Implement moving animation of the programmer through unity-chan.

---
+++ Assets/Scripts/Programmer/ProgrammerAnimationPresenter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+public class ProgrammerAnimationPresenter : MonoBehaviour
+{
+    [SerializeField]
+    private Programmer programmer;
+    [SerializeField]
+    private Animator animator;
+
+    private void Start()
+    {
+        programmer.OnMovingStarted += position =>
+        {
+            animator.Play("Moving");
+        };
+
+        programmer.OnMovingEnded += position =>
+        {
+            animator.SetTrigger("MovingEnd");
+        };
+    }
+}
