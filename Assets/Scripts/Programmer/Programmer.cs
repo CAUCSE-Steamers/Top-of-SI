0b7d7ae4ec91d6411a9ed71bacaeb98832186eb1
nErumin <nerumin90@gmail.com>
2018-09-30 19:46:40 +0900

    Implement moving logic of programmers.
    - Add testing scene for programmers.

---
+++ Assets/Scripts/Programmer/Programmer.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Programmer : MonoBehaviour
+{
+    public event Action<Vector3> OnMovingStarted = delegate { };
+    public event Action<Vector3> OnMovingEnded = delegate { };
+
+    public void Move(Vector3 deltaPosition)
+    {
+        StopCoroutine("StartMove");
+
+        StartCoroutine(StartMove(deltaPosition));
+    }
+
+    private IEnumerator StartMove(Vector3 deltaPosition)
+    {
+        var sourcePosition = transform.position;
+        var destinationPosition = transform.position + deltaPosition;
+
+        float interpolationValue = 0.0f;
+
+        OnMovingStarted(transform.position);
+
+        while (transform.position != destinationPosition)
+        {
+            yield return new WaitForEndOfFrame();
+            interpolationValue += Time.deltaTime;
+
+            var interpolatedPosition = Vector3.Lerp(sourcePosition, destinationPosition, interpolationValue);
+            transform.position = interpolatedPosition;
+        }
+
+        OnMovingEnded(transform.position);
+    }
+}
