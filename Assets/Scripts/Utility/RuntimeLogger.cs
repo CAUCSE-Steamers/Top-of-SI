dae156f35c97ebf4632c2824b04eaa269d17be9d
nErumin <nerumin90@gmail.com>
2018-10-02 02:59:31 +0900

    Add logger classes for seperating logging purposes.

---
+++ Assets/Scripts/Utility/RuntimeLogger.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+public static class RuntimeLogger
+{
+    public static void Log(object message)
+    {
+        Debug.Log(message);
+    }
+
+    public static void LogFormat(string format, params object[] args)
+    {
+        Debug.LogFormat(format, args);
+    }
+
+    public static void LogWarning(object message)
+    {
+        Debug.LogWarning(message);
+    }
+
+    public static void LogWarningFormat(string format, params object[] args)
+    {
+        Debug.LogWarningFormat(format, args);
+    }
+
+    public static void LogError(object message)
+    {
+        Debug.LogError(message);
+    }
+
+    public static void LogErrorFormat(string format, params object[] args)
+    {
+        Debug.LogErrorFormat(format, args);
+    }
+}
