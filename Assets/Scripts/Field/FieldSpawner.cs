c4a461971e95e1fa5f24dd128289be9a49c5108e
nErumin <nerumin90@gmail.com>
2018-10-08 23:20:56 +0900

    Can spawn multiple cells in order.

--- Assets/Scripts/Field/FieldSpawner.cs
+++ Assets/Scripts/Field/FieldSpawner.cs
@@ -1,4 +1,5 @@
 ﻿using UnityEngine;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 
@@ -7,34 +8,40 @@ public class FieldSpawner : MonoBehaviour
     private const string FieldObjectName = "Field";
 
     [SerializeField]
-    private Cell sampleCell;
+    private Cell[] sampleCells;
     [SerializeField]
     private Vector2Int fieldLength;
 
     private void Start()
     {
         SpawnField();
+        
+        foreach (var cell in sampleCells)
+        {
+            Destroy(cell.gameObject);
+        }
 
-        Destroy(sampleCell.gameObject);
         Destroy(this.gameObject);
     }
 
     private void SpawnField()
     {
         var fieldObject = CreateFieldObject();
-
         var includedCells = new List<List<Cell>>();
+        int pointedIndex = 0;
+
         for (int xPositionInField = 0; xPositionInField < fieldLength.x; ++xPositionInField)
         {
             var rowCells = new List<Cell>();
             for (int yPositionInField = 0; yPositionInField < fieldLength.y; ++yPositionInField)
             {
-                var createdCell = Instantiate(sampleCell, fieldObject.transform);
+                var createdCell = Instantiate(sampleCells[pointedIndex], fieldObject.transform);
                 
                 createdCell.transform.position = CalculateCellPosition(fieldObject.transform.position, xPositionInField, yPositionInField);
                 createdCell.gameObject.SetActive(true);
 
                 rowCells.Add(createdCell);
+                pointedIndex = (pointedIndex + 1) % sampleCells.Length;
             }
 
             includedCells.Add(rowCells);
@@ -54,8 +61,8 @@ public class FieldSpawner : MonoBehaviour
 
     private Vector3 CalculateCellPosition(Vector3 origin, int xPositionInField, int yPositionInField)
     {
-        return new Vector3(origin.x + xPositionInField * sampleCell.Size.x,
+        return new Vector3(origin.x + xPositionInField * sampleCells.First().Size.x,
                            origin.y,
-                           origin.z + yPositionInField * sampleCell.Size.y);
+                           origin.z + yPositionInField * sampleCells.First().Size.y);
     }
 }
