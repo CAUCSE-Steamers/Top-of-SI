56c6fb799c84dbec28087d1a2e59c97794da6ef0
nErumin <nerumin90@gmail.com>
2018-10-29 20:45:00 +0900

    Blocking action if a player already performed.

--- Assets/Scripts/Stage/ObjectUi/ObjectInformationPresenter.cs
+++ Assets/Scripts/Stage/ObjectUi/ObjectInformationPresenter.cs
@@ -12,9 +12,15 @@ public class ObjectInformationPresenter : MonoBehaviour
     private Transform skillPanelObject;
     [SerializeField]
     private SkillPresenter skillPresenterTemplate;
+    [SerializeField]
+    private GameObject actionBlockingObject;
 
     [SerializeField]
     private GameObject selectedEffectObject;
+
+    private void Start()
+    {
+    }
 
     public void SetObjectInformation(GameObject objectToView)
     {
@@ -29,10 +35,27 @@ public class ObjectInformationPresenter : MonoBehaviour
     public void ResetInformationUi()
     {
         selectedEffectObject.SetActive(false);
+        actionBlockingObject.SetActive(false);
 
-        foreach (var skillPresenter in skillPanelObject.GetComponentsInChildren<SkillPresenter>())
+        SetDefaultActionState(false);
+
+        foreach (var childButton in skillPanelObject.GetComponentsInChildren<Button>())
         {
-            Destroy(skillPresenter.gameObject);
+            if (childButton.GetComponent<SkillPresenter>() != null)
+            {
+                Destroy(childButton.gameObject);
+            }
+        }
+    }
+
+    private void SetDefaultActionState(bool newState)
+    {
+        foreach (var childButton in skillPanelObject.GetComponentsInChildren<Button>(true))
+        {
+            if (childButton.GetComponent<SkillPresenter>() == null)
+            {
+                childButton.gameObject.SetActive(newState);
+            }
         }
     }
 
@@ -47,11 +70,24 @@ public class ObjectInformationPresenter : MonoBehaviour
 
     private void RenderProgrammer(Programmer programmer)
     {
+        SetDefaultActionState(true);
+
+        RenderSelectionEffect(programmer);
+        RenderSkillPanel(programmer);
+    }
+
+    private void RenderSelectionEffect(Programmer programmer)
+    {
         var programmerPosition = programmer.transform.position;
 
         selectedEffectObject.SetActive(true);
         selectedEffectObject.transform.position =
             new Vector3(programmerPosition.x, programmerPosition.y + 0.5f, programmerPosition.z);
+    }
+
+    private void RenderSkillPanel(Programmer programmer)
+    {
+        actionBlockingObject.SetActive(StageManager.Instance.Unit.IsAbleToAct(programmer) == false);
 
         foreach (var activeSkill in programmer.Ability.AcquiredActiveSkills)
         {
