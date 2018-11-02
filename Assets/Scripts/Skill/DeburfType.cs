22da5fb60cea504506607b2d7413d4e8957f0140
RedChiken <gombabo1217@gmail.com>
2018-11-03 02:42:18 +0900

    Add DeburfType for Check Deburf type

---
+++ Assets/Scripts/Skill/DeburfType.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Model
+{
+    public enum DeburfType
+    {
+        None = 1, 
+        ShortenDeadLine = 2, 
+        IncreaseMentalUsage = 4, 
+        DisableMovement = 8, 
+        DecreaseDamage = 16
+    }
+}
