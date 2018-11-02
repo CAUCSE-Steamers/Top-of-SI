2a55ad8d230337b42d1c343c2a9929b37c9b600b
RedChiken <gombabo1217@gmail.com>
2018-11-03 06:13:49 +0900

    Add Burf function to Boss

--- Assets/Scripts/Boss/AbstractProject.cs
+++ Assets/Scripts/Boss/AbstractProject.cs
@@ -6,6 +6,7 @@ using Model;
 
 public abstract class AbstractProject : MonoBehaviour, IHurtable, IInvokeSkills
 {
+    private Boolean isOverwhelming = false, isDecreaseDamage = false;
     public event Action OnActionFinished = delegate { };
     public event Action OnDeath = delegate { };
 
@@ -27,7 +28,32 @@ public abstract class AbstractProject : MonoBehaviour, IHurtable, IInvokeSkills
         OnDeath();
     }
 
-    public ProjectSkill Invoke()
+    public void Burf(List<BurfStructure> burf)
+    {
+        foreach (var iter in burf)
+        {
+            switch (iter.Type)
+            {
+                case BurfType.Cure:
+                    int healedHP = (int)(Status.FullHealth * iter.Factor);
+                    Status.Health += healedHP;
+                    CommonLogger.Log("Boss Cured " + healedHP + "\n");
+                    break;
+                case BurfType.DecreaseDamage:
+                    // Pleace Check this before apply damage
+                    isDecreaseDamage = true;
+                    CommonLogger.Log("Damage is decreased " + iter.Factor * 100 + "%\n");
+                    break;
+                case BurfType.Overwhelming:
+                    // Please Check this before apply damage
+                    isOverwhelming = true;
+                    CommonLogger.Log("Boss is Overwhelming during " + iter.Turn + "turns\n");
+                    break;
+            }
+        }
+    }
+
+        public ProjectSkill Invoke()
     {
         ProjectSkill ret = Ability.InvokedSkill;
         anim.Play(ret.Information.Animation);
