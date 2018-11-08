20bc906fc00a5ae4626476a3202b9cc108b01951
RedChiken <gombabo1217@gmail.com>
2018-11-08 14:52:34 +0900

    Add Cost in ProgrammerStatus

---
+++ Assets/Scripts/Programmer/Money.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Model
+{
+    public class Money
+    {
+        public Money(int hire, int pay, int fire)
+        {
+            Hire = hire;
+            Pay = pay;
+            Fire = fire;
+        }
+        public int Hire
+        {
+            get; private set;
+        }
+        public int Pay
+        {
+            get; private set;
+        }
+        public int Fire
+        {
+            get; private set;
+        }
+    }
+}
