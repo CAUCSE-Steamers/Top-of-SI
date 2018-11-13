b95ffe35f0fd8fcc8bf704ae6e6a97b21296c63e
nErumin <nerumin90@gmail.com>
2018-11-14 03:07:20 +0900

    Synchronize with player's programmers.

---
+++ Assets/Scripts/Management/ManagementPresenter.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using Model;
+
+public class ManagementPresenter : MonoBehaviour
+{
+    [SerializeField]
+    private Text moneyText;
+    [SerializeField]
+    private ProgrammerListPresenter programmerListPresenter;
+
+    private Player currentPlayer;
+
+    private void Start()
+    {
+        currentPlayer = LobbyManager.Instance.CurrentPlayer;
+        Present();
+    }
+
+    private void Present()
+    {
+        moneyText.text = currentPlayer.Money.ToString();
+        programmerListPresenter.Present();
+    }
+}
