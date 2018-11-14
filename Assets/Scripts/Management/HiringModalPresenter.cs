6cb1c9637fc0db53363b6084887e729b53080a26
nErumin <nerumin90@gmail.com>
2018-11-14 17:23:43 +0900

    Implement hiring new programmer modal.

---
+++ Assets/Scripts/Management/HiringModalPresenter.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+using System.Collections;
+using Model;
+using UnityEngine.UI;
+
+public class HiringModalPresenter : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject hireSelectingObject;
+    [SerializeField]
+    private Text informationText;
+    [SerializeField]
+    private UnityEvent OnMoneyNotEnough;
+    [SerializeField]
+    private UnityEvent OnHiringConfirmed;
+
+    public void Present()
+    {
+        var newProgrammerSpec = new ProgrammerSpec();
+
+        informationText.text = string.Format("{0} 골드를 사용하여 새로운 프로그래머를 고용합니다.", newProgrammerSpec.Status.Cost.Hire);
+    }
+
+    public void ConfirmHire()
+    {
+        var newProgrammerSpec = new ProgrammerSpec();
+
+        if (LobbyManager.Instance.CurrentPlayer.Money < newProgrammerSpec.Status.Cost.Hire)
+        {
+            OnMoneyNotEnough.Invoke();
+        }
+        else
+        {
+            OnHiringConfirmed.Invoke();
+        }
+    }
+}
