723cb0d092e9976ca563e6f8824993625f5c6a45
nErumin <nerumin90@gmail.com>
2018-11-24 23:13:53 +0900

    Set the state when failure/victory.

--- Assets/Scripts/Stage/State/FailureState.cs
+++ Assets/Scripts/Stage/State/FailureState.cs
@@ -6,29 +6,8 @@ using Model;
 
 public class FailureState : DispatchableState
 {
-
-    // Use this for initialization
-    void Start()
-    {
-
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-
-    }
-
     protected override void ProcessEnterState()
     {
-
-    }
-
-    protected override void ProcessExitState()
-    {
-        // change scene
-
-
-        // destroy all managers in stage
+        StageManager.Instance.Status.CurrentStatus = StageStatus.Failure;
     }
 }
