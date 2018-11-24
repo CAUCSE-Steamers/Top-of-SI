420e39def1a819ec7452188557c51077f3a5dd99
RedChiken <computer6561@gmail.com>
2018-11-24 17:45:41 +0900

    Add Burfs

---
+++ Assets/Scripts/Programmer/Command/DamageSpreadBurf.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Model
+{
+    class DamageSpreadBurf : IBurf, IStatusModificationCommand
+    {
+        public DamageSpreadBurf(double ratio, double decreaseRatio)
+        {
+            SpreadRatio = ratio;
+            DecreaseRatio = decreaseRatio;
+        }
+
+        public double SpreadRatio
+        {
+            get; private set;
+        }
+
+        public double DecreaseRatio
+        {
+            get; private set;
+        }
+
+        public int RemainingTurn
+        {
+            get; set;
+        }
+
+        public string Description
+        {
+            get
+            {
+                return string.Format("자신이 받은 데미지의 {0} 배를 다른 프로그래머들에게 전달한다.", SpreadRatio);
+            }
+        }
+
+        public string IconName
+        {
+            get
+            {
+                return "Spread";
+            }
+
+        }
+
+        public bool IsPersistent
+        {
+            get
+            {
+                return true;
+            }
+        }
+
+        public bool IsPositiveBurf
+        {
+            get
+            {
+                return false;
+            }
+        }
+
+        public void Modify(ProgrammerStatus status)
+        {
+            
+        }
+
+        public void Unmodify(ProgrammerStatus status)
+        {
+            
+        }
+    }
+}
---
+++ Assets/Scripts/Programmer/Command/SocialityBurf.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Model
+{
+    class SocialityBurf : IBurf, IStatusModificationCommand
+    {
+        public SocialityBurf(int sociality)
+        {
+            Sociality = sociality;
+        }
+
+        public int Sociality
+        {
+            get; private set;
+        }
+
+        public int RemainingTurn
+        {
+            get; set;
+        }
+
+        public string Description
+        {
+            get
+            {
+                return string.Format("자신의 리더십을 {0} 증가시킨다.", Sociality);
+            }
+        }
+
+        public string IconName
+        {
+            get
+            {
+                return "Sociality";
+            }
+        }
+
+        public bool IsPersistent
+        {
+            get
+            {
+                return true;
+            }
+        }
+
+        public bool IsPositiveBurf
+        {
+            get
+            {
+                return true;
+            }
+        }
+
+        public void Modify(ProgrammerStatus status)
+        {
+            status.AddSociality(Sociality);
+            CommonLogger.LogFormat("LeadershipBurf::Modify => 프로그래머 '{0}'의 리더십이 {1}만큼 증가함.", status.Name, Sociality);
+        }
+
+        public void Unmodify(ProgrammerStatus status)
+        {
+            CommonLogger.LogFormat("LeadershipBurf::Modify => 프로그래머 '{0}'에 적용된 리더십 버프가 해제됨.", status.Name);
+        }
+    }
+}
