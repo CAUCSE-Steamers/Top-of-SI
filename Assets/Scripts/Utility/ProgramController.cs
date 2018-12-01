bc0458b368daad032a38d2539204968fb8402276
nErumin <nerumin90@gmail.com>
2018-12-01 16:37:35 +0900

    Load default player when starting.

--- Assets/Scripts/Utility/ProgramController.cs
+++ Assets/Scripts/Utility/ProgramController.cs
@@ -25,6 +25,10 @@ public class ProgramController : MonoBehaviour
         SceneManager.LoadScene(sceneName);
     }
 
+    public void LoadDefaultPlayer()
+    {
+        LobbyManager.Instance.LoadDefaultPlayer();
+    }
 
     public void LoadPlayer()
     {
